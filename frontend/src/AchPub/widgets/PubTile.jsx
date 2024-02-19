e92e87a32e245e8a59d50e09e8686ddfc6e892cd
Anadi Sharma <sharma.130@iitj.ac.in>
2024-02-19 20:19:48 +0530

    some files added

---
+++ frontend/src/AchPub/widgets/PubTile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PubTile() {
+  return (
+    <div className=''>
+        PubTile
+    </div>
+  )
+}
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div>Welcome to RAID's website</div>
+    <div>
+          Welcome to RAID's website
+          <iframe src="https://Supercaps-e8f1f460b3eb4570a26285472c17ac34?pvs=4"></iframe>
+
+    </div>
   )
 }
