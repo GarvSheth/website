9976e65fee936d3f7a5877f421cd5f129f5ee729
Shubh Goyal <goyal.shubh0711@gmail.com>
2024-02-22 18:11:38 +0530

    completes initial footer
    - intial footer design done
    - required links added

--- frontend/src/NavbarNFooter/Footer.jsx
+++ frontend/src/NavbarNFooter/Footer.jsx
@@ -5,13 +5,17 @@ export default function Footer() {
     return (
         <div className='footer'>
             <div className='text-container'>
-                <p>RAID, IIT Jodhpur</p>
-                <p>©2024  @all rights reserved</p>
+                <p className='footer-raid'>
+                    Realm of AI and Data, IIT Jodhpur
+                    <img src="IITJ_logo.png" alt="IIT Jodhpur" className='iitj-logo'/>
+                </p>
+                <p className='vertical-line'></p>
+                <p className='copyright'>©2024  @all rights reserved</p>
             </div>
             <div class="socials">
-                <div><a className='ri-instagram-line logo' href="https://www.instagram.com/raid.iitj/" target="_blank"></a></div>
-                <div><a className="ri-linkedin-fill logo" href="https://www.linkedin.com/company/raid-iitj/" target="_blank"></a></div>
-                <div><a className="ri-whatsapp-line logo" href="https://wa.me/918889722887" target="_blank"></a></div>
+                <div><a className='ri-instagram-line social-logo' href="https://www.instagram.com/raid.iitj/" target="_blank"></a></div>
+                <div><a className="ri-linkedin-fill social-logo" href="https://www.linkedin.com/company/raid-iitj/" target="_blank"></a></div>
+                <div><a className="ri-whatsapp-line social-logo" href="https://wa.me/918889722887" target="_blank"></a></div>
             </div>
         </div>
     )
